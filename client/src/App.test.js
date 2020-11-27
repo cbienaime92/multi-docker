c74f7fd20b291c3f571f06346ad42d219159f351
Christophe BIENAIME <cbienaime@gmail.com>
2020-11-27 02:27:23 +0100

    initial

---
+++ client/src/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+<<<<<<< HEAD
+test('renders learn react link', () => {});
+=======
+test('renders learn react link', () => {
+ 
+});
+>>>>>>> 9fa75db8e61d2df100dd3279c0bbaab93d76b9dd
