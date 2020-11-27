37f307fcbe41f7bf4f2f9ff4c07ee92b86a8a7bf
Christophe BIENAIME <cbienaime@gmail.com>
2020-11-27 02:49:59 +0100

    toto2

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,10 +2,4 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-<<<<<<< HEAD
 test('renders learn react link', () => {});
-=======
-test('renders learn react link', () => {
- 
-});
->>>>>>> 9fa75db8e61d2df100dd3279c0bbaab93d76b9dd
